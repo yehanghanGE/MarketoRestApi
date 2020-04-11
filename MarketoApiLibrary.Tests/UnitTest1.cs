524d9211453014a2243b936948e8f6b67672e919
yehanghanGE <yehang.han@ge.com>
2020-04-11 13:01:24 +0800

    Create Unit Tests Project for API

---
+++ MarketoApiLibrary.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MarketoApiLibrary.Tests
+{
+    public class UnitTest1
+    {
+        public void TestMethod1()
+        {
+        }
+    }
+}
