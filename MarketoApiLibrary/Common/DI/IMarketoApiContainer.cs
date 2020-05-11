06063e0e19751c0d9e5f86c1b15b7487b2e8644a
coderhh <yehanghan@gmail.com>
2020-05-11 22:05:56 +0800

    Add AutofacContainer.cs Add dependencies of AutofacContainer.cs

---
+++ MarketoApiLibrary/Common/DI/IMarketoApiContainer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketoApiLibrary.Common.DI
+{
+    public interface IMarketoApiContainer
+    {
+        bool IsInitialized { get; }
+        event EventHandler<MarketoApiContainerEventArgs> BeforeRegistrationCompletes;
+
+        void Initialize();
+
+        void RegisterType<T, U>(RegistrationLifetime registrationLifetime = RegistrationLifetime.InstancePerResolve) where U : T;
+        void RegisterGeneric(Type sourceType, Type targetType, RegistrationLifetime registrationLifetime = RegistrationLifetime.InstancePerResolve);
+        void RegisterInstance(Type T, object value);
+
+        T Resolve<T>(params IConstructorNamedParameter[] parameters);
+    }
+}
