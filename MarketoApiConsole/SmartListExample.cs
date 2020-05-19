5cd69bf5ca91465120b94144da4a80e1ca7c337e
coderhh <yehanghan@gmail.com>
2020-05-19 20:45:09 +0800

    Add SmartList Example

---
+++ MarketoApiConsole/SmartListExample.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MarketoApiLibrary.Asset.SmartLists;
+using MarketoApiLibrary.Common.Http.Oauth;
+using Newtonsoft.Json.Linq;
+
+namespace MarketoApiConsole
+{
+    public class SmartListExample
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+
+                var result = SmartList.GetSmartList();
+
+                Console.WriteLine(JToken.FromObject(result).ToString());
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
