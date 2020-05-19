c75b20f33bf1f0062474b0ac5ee76dc0be8ac768
coderhh <yehanghan@gmail.com>
2020-05-19 20:44:28 +0800

    Add Static Class Smartlist

---
+++ MarketoApiLibrary/Asset/SmartLists/SmartList.cs
@@ -0,0 +1,42 @@
+﻿using MarketoApiLibrary.Asset.SmartLists.Response;
+using MarketoApiLibrary.Common.DI;
+
+namespace MarketoApiLibrary.Asset.SmartLists
+{
+    public class SmartList
+    {
+        private static ISmartListController _smartListController;
+
+        public static ISmartListController SmartListController
+        {
+            get
+            {
+                if (_smartListController == null)
+                {
+                    Initialize();
+                }
+
+                return _smartListController;
+            }
+        }
+
+
+        static SmartList()
+        {
+            Initialize();
+        }
+
+        private static void Initialize()
+        {
+            _smartListController = MarketoApiContainer.Resolve<ISmartListController>();
+        }
+        /// <summary>
+        /// GET /rest/asset/v1/smartLists.json
+        /// </summary>
+        /// <returns></returns>
+        public static SmartListResponseWithRules GetSmartList()
+        {
+            return SmartListController.GetSmartLists<SmartListResponseWithRules>().Result;
+        }
+    }
+}
