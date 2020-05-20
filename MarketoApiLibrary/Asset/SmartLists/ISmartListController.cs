004d85dbe0f54033e821c0194745dd28c70d2033
coderhh <yehanghan@gmail.com>
2020-05-20 13:05:23 +0800

    Remove Async From Interface

--- MarketoApiLibrary/Asset/SmartLists/ISmartListController.cs
+++ MarketoApiLibrary/Asset/SmartLists/ISmartListController.cs
@@ -5,6 +5,6 @@ namespace MarketoApiLibrary.Asset.SmartLists
 {
     public interface ISmartListController
     {
-        Task<SmartListsResponse> GetSmartLists<T>();
+        SmartListsResponse GetSmartLists<T>();
     }
 }
