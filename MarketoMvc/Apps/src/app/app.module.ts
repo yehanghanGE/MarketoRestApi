646e6353b7bf70180d6b42ce86c6fbbcf2b8ac8c
coderhh <yehanghan@gmail.com>
2020-06-11 17:18:55 +0800

    Web API Angular POC
    Use Web Components in ASP.NET Project

---
+++ MarketoMvc/Apps/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, Injector } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
+
+import { AppComponent } from './app.component';
+import { CurrentTimeComponent } from './current-time/current-time.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CurrentTimeComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  entryComponents: [CurrentTimeComponent]
+})
+
+
+export class AppModule {
+  constructor(private injector: Injector) {
+  }
+  
+  ngDoBootstrap(){
+    customElements.define('current-time', createCustomElement(CurrentTimeComponent,
+                                                        {injector: this.injector}));
+  }
+}
+
+
