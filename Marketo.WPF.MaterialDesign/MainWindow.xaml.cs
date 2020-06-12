44e8c1b61d534eeff82c877415804cbe895a10ad
coderhh <yehanghan@gmail.com>
2020-06-12 18:10:47 +0800

    Desktop WPF Poc

---
+++ Marketo.WPF.MaterialDesign/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Marketo.WPF.MaterialDesign
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            var index = int.Parse(((Button)e.Source).Uid);
+
+            GridCursor.Margin = new Thickness(10 + (150 * index), 0, 0, 0);
+            switch (index)
+            {
+                case 0:
+                    GridMain.Background = Brushes.Aquamarine;
+                    break;
+                case 1:
+                    GridMain.Background = Brushes.Beige;
+                    break;
+                case 2:
+                    GridMain.Background = Brushes.CadetBlue;
+                    break;
+                case 3:
+                    GridMain.Background = Brushes.DarkBlue;
+                    break;
+                case 4:
+                    GridMain.Background = Brushes.Firebrick;
+                    break;
+                case 5:
+                    GridMain.Background = Brushes.Gainsboro;
+                    break;
+                case 6:
+                    GridMain.Background = Brushes.HotPink;
+                    break;
+            }
+        }
+
+        private void CloseBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
