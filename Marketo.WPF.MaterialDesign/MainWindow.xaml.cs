c072431fc2e1aae69025c816e61af9e750dcf867
coderhh <yehanghan@gmail.com>
2020-06-12 19:44:47 +0800

    Desktop WPF Poc
    Set Up Basic MVVM

--- Marketo.WPF.MaterialDesign/MainWindow.xaml.cs
+++ Marketo.WPF.MaterialDesign/MainWindow.xaml.cs
@@ -1,6 +1,6 @@
-﻿using System.Windows;
+﻿using Marketo.WPF.MaterialDesign.ViewModels;
+using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Media;
 
 namespace Marketo.WPF.MaterialDesign
 {
@@ -12,6 +12,7 @@ namespace Marketo.WPF.MaterialDesign
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new MainViewModel();
         }
 
         private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
@@ -19,30 +20,6 @@ namespace Marketo.WPF.MaterialDesign
             var index = int.Parse(((Button)e.Source).Uid);
 
             GridCursor.Margin = new Thickness(10 + (150 * index), 0, 0, 0);
-            switch (index)
-            {
-                case 0:
-                    GridMain.Background = Brushes.Aquamarine;
-                    break;
-                case 1:
-                    GridMain.Background = Brushes.Beige;
-                    break;
-                case 2:
-                    GridMain.Background = Brushes.CadetBlue;
-                    break;
-                case 3:
-                    GridMain.Background = Brushes.DarkBlue;
-                    break;
-                case 4:
-                    GridMain.Background = Brushes.Firebrick;
-                    break;
-                case 5:
-                    GridMain.Background = Brushes.Gainsboro;
-                    break;
-                case 6:
-                    GridMain.Background = Brushes.HotPink;
-                    break;
-            }
         }
 
         private void CloseBtn_OnClick(object sender, RoutedEventArgs e)
