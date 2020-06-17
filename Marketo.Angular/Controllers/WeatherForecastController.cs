0c26fa9d713ffa53a3101acef7cac234b893a26c
coderhh <yehanghan@gmail.com>
2020-06-17 10:07:07 +0800

    Rename MarketoWebClient to Maketo.Angular

--- Marketo.Angular/Controllers/WeatherForecastController.cs
+++ Marketo.Angular/Controllers/WeatherForecastController.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
-namespace MarketoWebClient.Controllers
+namespace Marketo.Angular.Controllers
 {
     [ApiController]
     [Route("[controller]")]
--- Marketo.Angular/WeatherForecast.cs
+++ Marketo.Angular/WeatherForecast.cs
@@ -1,6 +1,6 @@
 using System;
 
-namespace MarketoWebClient
+namespace Marketo.Angular
 {
     public class WeatherForecast
     {
