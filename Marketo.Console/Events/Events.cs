214eb6a9fea9388356c7be1c64f74d9c0c7fdd86
yehanghanGE <yehang.han@cytiva.com>
2021-03-09 20:53:47 +0800

    remove used test

--- Marketo.Console/Events/Events.cs
+++ Marketo.Console/Events/Events.cs
@@ -2,19 +2,19 @@
 {
     public class Events
     {
-        static void Main()
-        {
-            var pub = new Publisher();
+        //static void Main()
+        //{
+        //    var pub = new Publisher();
 
-            var sub1 = new Subscriber("sub1", pub);
-            var sub2 = new Subscriber("sub2", pub);
+        //    var sub1 = new Subscriber("sub1", pub);
+        //    var sub2 = new Subscriber("sub2", pub);
 
-            // Call the method that raise the event
-            pub.DoSomething();
+        //    // Call the method that raise the event
+        //    pub.DoSomething();
 
-            // Keep the console window open
-            System.Console.WriteLine("Press any key to continue...");
-            System.Console.ReadLine();
-        }
+        //    // Keep the console window open
+        //    System.Console.WriteLine("Press any key to continue...");
+        //    System.Console.ReadLine();
+        //}
     }
 }
